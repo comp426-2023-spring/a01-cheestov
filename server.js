a6ee238a54d52c06a299e9d0daf4edb4f490cf46
Cat Tovey <cheestov@email.unc.edu>
2023-01-19 10:09:41 -0500

    playing with vim

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 // Require fs module
 
-// Require minimist module (make sure you install this one via npm).
+// Require minimist module (make sure you install this one via npm) I did this in vim
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
