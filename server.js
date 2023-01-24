9418f6d1a497a20e486e659f34f0ba8c57c7a14f
Cat Tovey <cheestov@email.unc.edu>
2023-01-23 22:33:20 -0500

    got rid of some debugging console.log()s

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ var args = minimist(process.argv.slice(2), {
     integer: "port",
     default: {port: 3000 },
 });
-console.log(args.port);
+
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
 
 const port = args.port;
-console.log(port);
+
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
