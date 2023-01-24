8a452062735af1eab046ca7f408771598bf3e6b8
Cat Tovey <cheestov@email.unc.edu>
2023-01-23 21:51:56 -0500

    added fs module code, put everything in the read file function.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { hostname } = require('os');
 
 
 // Require fs module
+const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm) I did this in vim
 
@@ -23,33 +24,40 @@ const port = 3000;
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
+fs.readFile('public/index.html', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
 
 
 
 
 
-// Define a const `server` as an arrow function using http.createServer. 
-// Use the documentation for the node.js http module. 
-// The function should have three responses: 
-// 1. status code 200, 
-// 2. set a header with content type `text/html`, and 
-// 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end("hello world");
+
+    // Define a const `server` as an arrow function using http.createServer. 
+    // Use the documentation for the node.js http module. 
+    // The function should have three responses: 
+    // 1. status code 200, 
+    // 2. set a header with content type `text/html`, and 
+    // 3. end with the data that you are reading in from ./public/index.html.
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain');
+        res.end(data);
+    });
+
+
+
+
+
+    // Start the `server` const listening on the port defined by argument in your `port` const. 
+    // Put the exact message `Server listening on port ${port}` on the console log. 
+
+    server.listen(port, () => {
+        console.log('Server listening on port ${port}');
+    });
+
 });
-
-
-
-
-
-// Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log. 
-
-server.listen(port, () => {
-    console.log('Server listening on port ${port}');
-});
-
 
 // That's it! You're all done!
