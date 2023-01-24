a5a9978b3830e3f43ed761bf786ff773e7c24333
Cat Tovey <cheestov@email.unc.edu>
2023-01-23 21:34:36 -0500

    Finished with http module?

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Require http module 
 const http = require('http');
+const { hostname } = require('os');
+
+
 
 // Require fs module
 
@@ -9,6 +12,9 @@ const http = require('http');
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
+
+
+const port = 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
@@ -31,7 +37,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
-    //res.end()
+    res.end("hello world");
 });
 
 
@@ -41,7 +47,9 @@ const server = http.createServer((req, res) => {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-
+server.listen(port, () => {
+    console.log('Server listening on port ${port}');
+});
 
 
 // That's it! You're all done!
